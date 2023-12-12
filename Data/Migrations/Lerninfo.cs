9e49fd1b858fca523a1fed52e744aa654fc07a34
Stephan Kansy <stkansy@gmail.com>
2023-12-12 12:39:25 +0100

    update database

---
+++ Data/Migrations/Lerninfo.cs
@@ -0,0 +1,10 @@
+﻿namespace JokesWebApp.Data.Migrations
+{
+    public class Lerninfo
+    {
+        // Language of creating tables commands for pm:
+
+        // add-migration "intialsetup" (ersterzeugung table)
+        // update-database (macht die tables user acc und jokes)
+    }
+}
